927b23e956ef4b4f49f81513a04cb44164239e42
twideem <twideem@outlook.com>
2021-05-30 03:06:59 +0800

    Periodic updates

--- objects/plugins/checkin.js
+++ objects/plugins/checkin.js
@@ -1,1 +1,1 @@
-function checkin(){if('2021-05-21 10:32:49'.substr(8,2)!=new Date().getDate()) document.body.style.backgroundColor='LIGHTPINK'}
+function checkin(){if('2021-05-29 19:00:38'.substr(8,2)!=new Date().getDate()) document.body.style.backgroundColor='LIGHTPINK'}
